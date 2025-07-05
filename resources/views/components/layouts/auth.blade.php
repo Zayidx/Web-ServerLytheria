64aef53aabb0ec1b6d95ac334a7bc2c0dba5c5f1
Farid Indrawan <128061930+Zayidx@users.noreply.github.com>
2025-07-05 15:57:20 +0700

    update tailwind

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Lytheria SMP - Dunia Petualangan Baru' }}</title>
     
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
